61e398d2fcaf1c6459c0a9833e11a124a989d674
Martin Ales <martin@gcoop.coop>
2019-11-03 16:59:03 -0300

    Keep only the rotate function and improve UI

--- ScreenRotator.py
+++ ScreenRotator.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# /usr/bin/env python3
 
 import os
 import signal
@@ -10,33 +10,24 @@ APPINDICATOR_ID = "screenrotator"
 orientation = "normal"
 
 def main():
+    global indicator
     indicator = AppIndicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('./icon.svg'), AppIndicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator.set_label("Horizontal", "Horizontal")
     indicator.set_status(AppIndicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
     Gtk.main()
 
 def build_menu():
     menu = Gtk.Menu()
-    #brightness
-    item_brightness_up = Gtk.MenuItem('Increase Brightness')
-    item_brightness_up.connect('activate', increase_brightness)
-    menu.append(item_brightness_up)
-    item_brightness_down = Gtk.MenuItem("Decrease Brightness")
-    item_brightness_down.connect('activate', decrease_brightness)
-    menu.append(item_brightness_down)
     #rotate
-    item_rotate = Gtk.MenuItem('Rotate')
+    item_rotate = Gtk.MenuItem('Rotar pantalla')
     item_rotate.connect('activate', rotate_screen)
     menu.append(item_rotate)
-    #flip
-    item_flip = Gtk.MenuItem('Flip')
-    item_flip.connect('activate', flip_screen)
-    menu.append(item_flip)
     #seperator
     seperator = Gtk.SeparatorMenuItem()
     menu.append(seperator)
     #quit
-    item_quit = Gtk.MenuItem('Quit')
+    item_quit = Gtk.MenuItem('Cerrar')
     item_quit.connect('activate', quit)
     menu.append(item_quit)
     menu.show_all()
@@ -45,27 +36,15 @@ def build_menu():
 def rotate_screen(source):
     global orientation
     if orientation == "normal":
+        indicator.set_label("Vertical", "Horizontal")
+        source.set_label("Rotar pantalla (Vertical)")
         direction = "left"
     elif orientation == "left":
+        indicator.set_label("Horizontal", "Horizontal")
+        source.set_label("Rotar pantalla (Horizontal)")
         direction ="normal"
     call(["xrandr", "-o", direction])
     orientation = direction
-
-def flip_screen(source):
-    global orientation
-    if orientation == "normal":
-        direction = "inverted"
-    elif orientation == "inverted":
-        direction ="normal"
-    call(["xrandr", "-o", direction])
-    orientation = direction
-
-
-def increase_brightness(source):
-    call(["xbacklight", "-inc", "20"])
-
-def decrease_brightness(source):
-    call(["xbacklight", "-dec", "20"])
 
 if __name__ == "__main__":
     #make sure the screen is in normal orientation when the script starts
