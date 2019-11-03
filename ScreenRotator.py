158cbfb8cd10757799ac9f8e8d280b1cef612918
Martin Ales <martin@gcoop.coop>
2019-11-03 17:31:53 -0300

    Ensure abs path for icon
    Warnings fixed

--- ScreenRotator.py
+++ ScreenRotator.py
@@ -3,7 +3,10 @@
 import os
 import signal
 from subprocess import call
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+gi.require_version('AppIndicator3', '0.1')
 from gi.repository import AppIndicator3 as AppIndicator
 
 APPINDICATOR_ID = "screenrotator"
@@ -11,7 +14,9 @@ orientation = "right"
 
 def main():
     global indicator
-    indicator = AppIndicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('./icon.svg'), AppIndicator.IndicatorCategory.SYSTEM_SERVICES)
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    icon_path = script_path + '/icon.svg'
+    indicator = AppIndicator.Indicator.new(APPINDICATOR_ID, icon_path, AppIndicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_label("Horizontal", "Horizontal")
     indicator.set_status(AppIndicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
@@ -20,14 +25,14 @@ def main():
 def build_menu():
     menu = Gtk.Menu()
     #rotate
-    item_rotate = Gtk.MenuItem('Rotar pantalla')
+    item_rotate = Gtk.MenuItem(label='Rotar pantalla')
     item_rotate.connect('activate', rotate_screen)
     menu.append(item_rotate)
     #seperator
     seperator = Gtk.SeparatorMenuItem()
     menu.append(seperator)
     #quit
-    item_quit = Gtk.MenuItem('Cerrar')
+    item_quit = Gtk.MenuItem(label='Cerrar')
     item_quit.connect('activate', quit)
     menu.append(item_quit)
     menu.show_all()
